97000bc15db07e073764201432e0dfab8c8e1262
unknown <you@example.com>
2022-06-24 10:47:53 +0200

    [Integrating the Back-end] ajout des dependances dans guestbook-web et dans GuestbookPortletKey

--- modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
+++ modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
@@ -1,12 +1,14 @@
 package com.liferay.docs.guestbook.portlet.portlet;
 
-import com.liferay.docs.guestbook.portlet.constants.GuestbookPortletKeys;
-
-import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-
 import javax.portlet.Portlet;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.liferay.docs.guestbook.portlet.constants.GuestbookPortletKeys;
+import com.liferay.docs.guestbook.service.GuestbookEntryLocalService;
+import com.liferay.docs.guestbook.service.GuestbookLocalService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
 /**
  * @author Evoleen
@@ -22,10 +24,17 @@ import org.osgi.service.component.annotations.Component;
 		      "javax.portlet.init-param.template-path=/",
 		      "javax.portlet.init-param.view-template=/guestbook/view.jsp",
 		      "javax.portlet.resource-bundle=content.Language",
+		      "javax.portlet.name=" + GuestbookPortletKeys.GUESTBOOK,
 		      "javax.portlet.security-role-ref=power-user,user",
 		      "javax.portlet.supports.mime-type=text/html"
 	},
 	service = Portlet.class
 )
 public class GuestbookPortlet extends MVCPortlet {
+	@Reference
+	private GuestbookEntryLocalService _guestbookEntryLocalService;
+
+	@Reference
+	private GuestbookLocalService _guestbookLocalService;
+
 }
