19d6ed47bd7b080d46b1cfcca4f8326f4d1ca25b
unknown <you@example.com>
2022-06-28 10:00:56 +0200

    [Implementing Portlet Actions] ajout des sources dans GuestbookPortlet

--- modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
+++ modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
@@ -1,14 +1,22 @@
 package com.liferay.docs.guestbook.portlet.portlet;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import com.liferay.docs.guestbook.model.GuestbookEntry;
 import com.liferay.docs.guestbook.portlet.constants.GuestbookPortletKeys;
 import com.liferay.docs.guestbook.service.GuestbookEntryLocalService;
 import com.liferay.docs.guestbook.service.GuestbookLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 
 /**
  * @author Evoleen
@@ -36,5 +44,61 @@ public class GuestbookPortlet extends MVCPortlet {
 
 	@Reference
 	private GuestbookLocalService _guestbookLocalService;
+	
+	public void addEntry(ActionRequest request, ActionResponse response)
+            throws PortalException {
+
+        ServiceContext serviceContext = ServiceContextFactory.getInstance(
+            GuestbookEntry.class.getName(), request);
+
+        String userName = ParamUtil.getString(request, "name");
+        String email = ParamUtil.getString(request, "email");
+        String message = ParamUtil.getString(request, "message");
+        long guestbookId = ParamUtil.getLong(request, "guestbookId");
+        long entryId = ParamUtil.getLong(request, "entryId");
+
+    if (entryId > 0) {
+
+        try {
+
+            _guestbookEntryLocalService.updateGuestbookEntry(
+                serviceContext.getUserId(), guestbookId, entryId, userName,
+                email, message, serviceContext);
+
+            response.setRenderParameter(
+                "guestbookId", Long.toString(guestbookId));
+
+        }
+        catch (Exception e) {
+            System.out.println(e);
+
+            PortalUtil.copyRequestParameters(request, response);
+
+            response.setRenderParameter(
+                "mvcPath", "/guestbook/edit_entry.jsp");
+        }
+
+    }
+    else {
+
+        try {
+            _guestbookEntryLocalService.addGuestbookEntry(
+                serviceContext.getUserId(), guestbookId, userName, email,
+                message, serviceContext);
+
+            response.setRenderParameter(
+                "guestbookId", Long.toString(guestbookId));
+
+        }
+        catch (Exception e) {
+            System.out.println(e);
+
+            PortalUtil.copyRequestParameters(request, response);
+
+            response.setRenderParameter(
+                "mvcPath", "/guestbook/edit_entry.jsp");
+        }
+    }
+}
 
 }
